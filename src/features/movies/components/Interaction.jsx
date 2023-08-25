ac91d19ff95889245179ef716292126912c20d6a
James Banus <jamesbanus@Jamess-MacBook-Pro.local>
2023-08-25 17:06:08 +0100

    more styling

--- src/features/movies/components/Interaction.jsx
+++ src/features/movies/components/Interaction.jsx
@@ -1,6 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
 import { openModal } from "../modalSlice";
 import play from "./../../../assets/play.png";
+import favourite from "./../../../assets/favourite.png";
+import favouriteFilled from "./../../../assets/favouriteFilled.png";
+import star from "./../../../assets/star.png";
+import starFilled from "./../../../assets/starFilled.png";
 
 const Interaction = () => {
   const dispatch = useDispatch();
@@ -8,13 +12,32 @@ const Interaction = () => {
   return (
     <>
       <div className="interactionContainer">
+        <div className="ratingsDiv">
+          <img src={star} alt="star" className="ratings" />
+          <img src={star} alt="star" className="ratings" />
+          <img src={star} alt="star" className="ratings" />
+          <img src={star} alt="star" className="ratings" />
+          <img src={star} alt="star" className="ratings" />
+          <div className="ratingsInnerDiv">
+            <img src={starFilled} alt="starFilled" className="ratingsFilled" />
+            <img src={starFilled} alt="starFilled" className="ratingsFilled" />
+            <img src={starFilled} alt="starFilled" className="ratingsFilled" />
+            <img src={starFilled} alt="starFilled" className="ratingsFilled" />
+            <img src={starFilled} alt="starFilled" className="ratingsFilled" />
+          </div>
+        </div>
+        <div className="favouriteDiv">
+          <img src={favourite} alt="favourite" className="favourite" />
+        </div>
         <div className="playButtonDiv">
           <img
             className="playButton"
             onClick={() => dispatch(openModal())}
             src={play}
           />
-          <p onClick={() => dispatch(openModal())}>Play Trailer</p>
+          <div className="wording">
+            <p onClick={() => dispatch(openModal())}>Play Trailer</p>
+          </div>
         </div>
       </div>
     </>
